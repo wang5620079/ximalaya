f5791cf4427e5b0a5eb41b580233ff54a367624a
wang5620079 <wang5620079@163.com>
2018-10-12 11:51:14 +0800

    喜马拉雅 下载器

---
+++ config.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+# -*- coding: utf-8 -*-
+
+import logging
+
+"""
+配置文件
+"""
+
+#日志配置
+gloableloglevel = logging.DEBUG
+fileloglevel = logging.INFO
+consoleloglevel = logging.DEBUG
+
+#url列表文件名称
+urlsconfig='urls.txt'
